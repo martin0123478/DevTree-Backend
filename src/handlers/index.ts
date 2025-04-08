27a942393eb4ed0f532090350cbc5c7ded508db7
martin0123478 <martin_agui97@outlook.es>
2025-04-07 21:35:56 -0600

    feat:hashear passwords in database

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from 'express'
 import User from "../models/User"
+import { hashPasword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response): Promise<void> => {
-    const { email } = req.body
+    const { email, password } = req.body
     const emailExist = await User.findOne({ email })
     if (emailExist) {
         const error = new Error('El usuario ya está registrado')
         res.status(409).json({ error: error.message })
         return
     }
-
-    await User.create(req.body)
+    const user = new User(req.body)
+    user.password = await hashPasword(password)
+    await user.save()
     res.status(201).json({ msg: 'Usuario Registrado Corectamente' })
 }
