f7df2a77bc99a875d0f35dbb21bb2b2afdf1b9bc
martin0123478 <martin_agui97@outlook.es>
2025-04-09 19:22:44 -0600

    feat:validate body with express validator

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { createAccount } from './handlers'
 
 const router = Router()
 
 //Atenticación y registro
-router.post('/auth/register', createAccount)
+router.post('/auth/register',
+
+    body('handle').notEmpty().withMessage('El handle es obligatorio'),
+    body('email').isEmail().withMessage('Email no valido'),
+    body('password').isLength({ min: 8 }).withMessage('Password muy corto, el minimo es de 8 ')
+    , createAccount)
 
 export default router
